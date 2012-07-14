58c31c801fe3e87a47e82976f731a69d17ca5d4b
Steve Schwartz <steve@alfajango.com>
2012-07-14 05:32:50 -0400

    Added admin ability to create new clients and users

--- routes/admin.js
+++ routes/admin.js
@@ -28,15 +28,27 @@ module.exports = function(app) {
   });
 
   app.get('/admin/create_user', auth.ensureAuthenticated, auth.ensureAdmin, function(req, res) {
-    res.render('admin_create_user', {
-      title: 'Admin',
-      message: req.flash()
+    Client.find({}, function(err, clients) {
+      res.render('admin_create_user', {
+        title: 'Admin',
+        message: req.flash(),
+        clients: clients
+      });
     });
   });
 
   app.post('/admin/create_user', auth.ensureAuthenticated, auth.ensureAdmin, function(req, res) {
-    // Create User
-    res.redirect('/admin/create_user');
+    new User(req.body.user).save( function(err, user) {
+      if (err) {
+        res.render('admin_create_user', {
+          title: 'Admin',
+          message: { error: 'User could not be saved: ' + err }
+        });
+      } else {
+        req.flash('info', "Success!");
+        res.redirect('/admin');
+      }
+    });
   });
 
   app.get('/admin/create_client', auth.ensureAuthenticated, auth.ensureAdmin, function(req, res) {
@@ -47,7 +59,16 @@ module.exports = function(app) {
   });
 
   app.post('/admin/create_client', auth.ensureAuthenticated, auth.ensureAdmin, function(req, res) {
-    // Create Client
-    res.redirect('/admin');
+    new Client(req.body.client).save( function(err, client) {
+      if (err) {
+        res.render('admin_create_client', {
+          title: 'Admin',
+          message: { error: 'Client could not be saved: ' + err }
+        });
+      } else {
+        req.flash('info', "Success!");
+        res.redirect('/admin');
+      }
+    });
   });
 };
