818af480e0ba645f01d015bf23c90cd735c7f104
Steve Schwartz <steve@alfajango.com>
2012-07-22 23:36:30 -0400

    Reformatted errbit errors layout to be simpler

--- app/widgets/errbit_unresolved_exceptions/update.js
+++ app/widgets/errbit_unresolved_exceptions/update.js
@@ -3,8 +3,8 @@ widgets.errbit_unresolved_exceptions = function(data, $) {
   if (data.errbit_unresolved_exceptions.length > 0) {
     $.each(data.errbit_unresolved_exceptions, function(i, exception) {
       rows += '<tr>';
-      rows += '<td class="errbit-description-td"><button class="btn btn-mini pull-right errbit-description-toggle">&hellip;</button><div class="errbit-url">' + (exception.url && exception.url !== "{}" ? exception.url : 'n/a') + '</div><br class="clear" /><div class="errbit-description"><pre>' + exception.error_class + '\n' + JSON.stringify(exception.messages) + '</pre></div></td>';
-      rows += '<td>' + exception.env + '</td>';
+      rows += '<td>' + exception.env + '<br />';
+      rows += (exception.url && exception.url !== "{}" ? stringToUrl(exception.url).pathname : 'n/a') + '</td>';
       rows += '<td>' + exception.count + '</td>';
       rows += '<td>' + (humaneDate(exception.last_occurrence) || exception.last_occurrence) + '</td>';
       rows += '</tr>';
