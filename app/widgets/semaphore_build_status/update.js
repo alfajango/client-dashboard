c1d69876cb58574b0344e85293d9c0fdc2d4434b
Steve Schwartz <steve@alfajango.com>
2012-07-29 22:17:43 -0400

    Restructured widget layout

--- app/widgets/semaphore_build_status/update.js
+++ app/widgets/semaphore_build_status/update.js
@@ -6,7 +6,7 @@ widgets.semaphore_build_status = function(data, $) {
   } else {
     var out = "";
     out += (passed ? '<div class="alert alert-success">' : '<div class="alert alert-error">');
-    out += 'Test suite for ' + data.results.project_name + '[' + data.results.branch_name + '] ' + data.results.status;
+    out += data.results.project_name + ' [' + data.results.branch_name + '] test suite ' + data.results.status;
     out += (passed ? ' <i class="icon-thumbs-up"></i>' : ' <i class="icon-exclamation-sign"></i>');
     $target.find('.semaphore-build-status').html(out);
     $target.find('.refresh-service[data-service="semaphore_build_status"]').removeClass('disabled').html('<i class="icon-retweet"></i>').siblings('.refresh-ok').show().delay('250').fadeOut();
