23bd654cf3a036b2f4fa22bc88c2610e5eab8d3f
Steve Schwartz <steve@alfajango.com>
2012-08-29 17:45:52 -0400

    AJ-624 Fixed money formatting for cashboard amounts.

--- app/widgets/cashboard_uninvoiced_amounts/update.js
+++ app/widgets/cashboard_uninvoiced_amounts/update.js
@@ -4,8 +4,8 @@ widgets.cashboard_uninvoiced_amounts = function(data, $) {
     $target.find('.uninvoiced-time').html('<div class="alert alert-error" title="' + data.error + '">There was a problem retrieving amount</div>');
     $target.find('.uninvoiced-expenses').html('');
   } else {
-    $target.find('.uninvoiced-time').html('$' + addCommas(data.results.invoice));
-    $target.find('.uninvoiced-expenses').html('$' + addCommas(data.results.expenses));
+    $target.find('.uninvoiced-time').html('$' + data.results.invoice.formatMoney(2, '.', ','));
+    $target.find('.uninvoiced-expenses').html('$' + data.results.expenses.formatMoney(2, '.', ','));
     $target.find('.refresh-service[data-service="cashboard_uninvoiced_amounts"]').removeClass('disabled').html('<i class="icon-retweet"></i>').siblings('.refresh-ok').show().delay('250').fadeOut();
   }
 };
