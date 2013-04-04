a6891b40a92062c25497549f8b55aeed0a72639d
Steve Schwartz <steve@alfajango.com>
2013-04-04 14:10:29 -0400

    Added flot plotting for cashboard global billable widget. Reformatted cashboard global widget.

--- app/widgets/cashboard_global_billable_time/update.js
+++ app/widgets/cashboard_global_billable_time/update.js
@@ -1,20 +1,41 @@
 widgets.cashboard_global_billable_time = function(data, $) {
+  function group(obj, key, value) {
+    if (!(obj[key])) {
+      obj[key] = value;
+    } else {
+      if (typeof(value) === "number") {
+        obj[key] = obj[key] + value;
+      } else {
+        Object.keys(value).forEach(function(subKey) {
+          group(obj[key], subKey, value[subKey]);
+        });
+      }
+    }
+  }
   var $target = $('#widget-' + data.id),
       rows = "";
   if (data.results && data.results.length > 0) {
-    var totalHours = 0,
+    var hoursByRate = {},
+        hoursByProject = {},
+        hoursByMember = {},
+        hoursByDayByMember = {},
+        hoursByDayByProject = {},
+        hoursByMemberByDay = {},
+        totalHours = 0,
         totalBillable = 0;
     $.each(data.results, function(i, entry) {
-      var created = new Date(entry.created_on),
-          createdInt = +(created), // Make sure both dates are compared as integers
+      var createdAt = new Date(entry.created_on),
           hours = entry.minutes / 60.0,
           rate = parseFloat(entry.billable_rate),
           billable = hours * rate,
-          currDate = created.getDate(),
-          currMonth = created.getMonth() + 1, //Months are zero based
-          currYear = created.getFullYear();
+          currDate = createdAt.getDate(),
+          currMonth = createdAt.getMonth(), //Months are zero based
+          currYear = createdAt.getFullYear()
+          created = new Date(currYear, currMonth, currDate),
+          createdInt = +(created), // Make sure both dates are compared as integers
+          formattedDate = currYear + "-" + (currMonth + 1) + "-" + currDate;
       rows += '<tr' + (entry.rate <= 0 ? ' class="zero-rate"' : '') + '>';
-      rows += '<td>' + currDate + "-" + currMonth + "-" + currYear + '</td>';
+      rows += '<td>' + formattedDate + '</td>';
       rows += '<td>' + entry.person_name + '</td>';
       rows += '<td>' + entry.project_name + '</td>';
       rows += '<td>' + hours + '</td>';
@@ -25,10 +46,84 @@ widgets.cashboard_global_billable_time = function(data, $) {
 
       totalHours += hours;
       totalBillable += billable;
-    });
-    rows = '<tr class="totals"><th colspan=3>Total Hours: ' + totalHours + '</th><th colspan=3>Total Billable: $' + totalBillable.formatMoney(2, '.', ',') + '</th><th colspan=1>Avg Hourly Rate: $' + (totalBillable / totalHours).formatMoney(2, '.', ',') + '</th></tr>' + rows;
 
+      group(hoursByRate, rate, hours);
+      group(hoursByProject, entry.project_name, hours);
+      group(hoursByMember, entry.person_name, hours);
+
+      var personHours = {};
+      personHours[entry.person_name] = hours;
+      group(hoursByDayByMember, formattedDate, personHours)
+
+      var projectHours = {};
+      projectHours[entry.project_name] = hours;
+      group(hoursByDayByProject, formattedDate, projectHours)
+
+      var dayHours = {};
+      dayHours[createdInt] = billable;
+      group(hoursByMemberByDay, entry.person_name, dayHours)
+
+    });
+
+    console.log(hoursByRate);
+    console.log(hoursByProject);
+    console.log(hoursByMember);
+    console.log(hoursByDayByMember);
+    console.log(hoursByDayByProject);
+    console.log(hoursByMemberByDay);
+
+    $target.find('.total-hours').html(totalHours);
+    $target.find('.total-billable').html('$' + totalBillable.formatMoney(2, '.', ','));
+    $target.find('.average-hourly-rate').html('$' + (totalBillable / totalHours).formatMoney(2, '.', ','));
     $target.find('.cashboard-billable-table tbody').html(rows);
+
+    var startDateVal = $target.find('input[name="start_date"]').val().split('-'),
+        endDateVal = $target.find('input[name="end_date"]').val().split('-'),
+        startDate = new Date(startDateVal[0], (startDateVal[1] - 1), startDateVal[2]),
+        endDate = new Date(endDateVal[0], (endDateVal[1] - 1), endDateVal[2]),
+        startDateInt = +(startDate),
+        endDateInt = +(endDate);
+
+    var plotSeries = [],
+        members = Object.keys(hoursByMember);
+    for (var d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+      members.forEach(function(member) {
+        var memberIndex = members.indexOf(member),
+            day = new Date(d),
+            dayDate = day.getDate(),
+            dayMonth = day.getMonth(), //Months are zero based
+            dayYear = day.getFullYear()
+            formattedDay = dayYear + "-" + (dayMonth + 1) + "-" + dayDate;
+            dayInt = +(day),
+            hours = hoursByMemberByDay[member][dayInt] || 0,
+            value = [dayInt, hours];
+        if (plotSeries[memberIndex] === undefined) {
+          plotSeries[memberIndex] = [value];
+        } else {
+          plotSeries[memberIndex].push(value);
+        }
+      });
+    }
+
+    console.log(plotSeries);
+
+    $.plot($target.find('.hours-by-day-by-member'), plotSeries, {
+      series: {
+        stack: true,
+        lines: {
+          show: true,
+          fill: true,
+          steps: false
+        }
+      },
+      xaxis: {
+        mode: "time",
+        minTickSize: [1, "day"],
+        min: startDateInt,
+        max: endDateInt,
+        timeformat: "%a"
+      }
+    });
   } else if (data.error) {
     $target.find('.cashboard-billable-table tbody').html('<tr><td colspan=7><div class="alert alert-error" title="' + data.error + '">There was a problem retrieving amount</div></td></tr>');
   } else {
