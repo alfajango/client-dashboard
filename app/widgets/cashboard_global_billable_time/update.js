84e8a31d7a4775fe980f9d52e06b59811021bbcd
Steve Schwartz <steve@alfajango.com>
2013-04-10 14:18:35 -0400

    Added chart hover crosshairs to cashboard global widget charts.

--- app/widgets/cashboard_global_billable_time/update.js
+++ app/widgets/cashboard_global_billable_time/update.js
@@ -59,7 +59,7 @@ widgets.cashboard_global_billable_time = function(data, $) {
 
     plotSeries[0] = {
       color: "#ebc4c4",
-      label: "[break even]",
+      label: "[break even] -",
       stack: false,
       data: [],
       points: {
@@ -89,7 +89,7 @@ widgets.cashboard_global_billable_time = function(data, $) {
 
         if (plotSeries[keyIndex] === undefined) {
           plotSeries[keyIndex] = {
-            label: key,
+            label: key + ' -',
             data: [[dayInt, value]],
             hoverable: true
           };
@@ -105,7 +105,7 @@ widgets.cashboard_global_billable_time = function(data, $) {
 
     console.log(plotSeries);
 
-    $.plot($target, plotSeries, {
+    var plot = $.plot($target, plotSeries, {
       series: {
         stack: true,
         lines: {
@@ -134,9 +134,72 @@ widgets.cashboard_global_billable_time = function(data, $) {
         hoverable: true,
         clickable: true
       },
+      crosshair: {
+        mode: "x"
+      },
       legend: {
         show: true,
         position: cumulative ? "nw" : "ne"
+      }
+    });
+
+    var legends = $target.find(".legendLabel");
+
+    legends.each(function () {
+      // fix the widths so they don't jump around
+      //$(this).css('width', $(this).width() + 20);
+    });
+
+    var updateLegendTimeout = null;
+    var latestPosition = null;
+
+    function updateLegend() {
+
+      updateLegendTimeout = null;
+
+      var pos = latestPosition;
+
+      var axes = plot.getAxes();
+      if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+          pos.y < axes.yaxis.min || pos.y > axes.yaxis.max) {
+        return;
+      }
+
+      var i, j, dataset = plot.getData();
+      for (i = 0; i < dataset.length; ++i) {
+
+        var series = dataset[i];
+
+        // Find the nearest points, x-wise
+
+        for (j = 0; j < series.data.length; ++j) {
+          if (series.data[j][0] > pos.x) {
+            break;
+          }
+        }
+
+        // Now Interpolate
+
+        var y,
+        p1 = series.data[j - 1],
+        p2 = series.data[j];
+
+        if (p1 == null) {
+          y = p2[1];
+        } else if (p2 == null) {
+          y = p1[1];
+        } else {
+          y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+        }
+
+        legends.eq(i).text(series.label.replace(/-.*/, "- $" + y.toFixed(axes.yaxis.tickDecimals)));
+      }
+    }
+
+    $target.bind("plothover",  function (event, pos, item) {
+      latestPosition = pos;
+      if (!updateLegendTimeout) {
+        updateLegendTimeout = setTimeout(updateLegend, 50);
       }
     });
   }
