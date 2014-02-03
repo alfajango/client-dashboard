6c0e3a1d6c352e3908b39236964c5070f9c1934f
Steve Schwartz <steve@alfajango.com>
2014-02-02 23:30:20 -0500

    Added project-choose screen for clients with more than one project.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -20,13 +20,39 @@ module.exports = function(app) {
           if (client.projects.length === 1) {
             req.project = client.projects[0];
           } else {
-            req.flash('info', "Choose a project");
-            res.redirect('/choose');
+            var projectIds = client.projects.map( function(p) { return p.id; } );
+            if (req.query.project_id && projectIds.indexOf(req.query.project_id) > -1) {
+              req.project = _.find(client.projects, function(p) { return p.id == req.query.project_id });
+            } else {
+              req.flash('info', "Choose a project");
+              res.redirect('/choose?client_id=' + client.id);
+            }
           }
         } else {
           req.flash('warn', "No projects found for you");
           res.redirect('/login');
         }
+      }
+      next();
+    });
+  };
+
+  var getProjects = function(req, res, next) {
+    var clientQuery;
+    if (req.user.admin && req.query.client_id) {
+      clientQuery = Client.findById(req.query.client_id);
+    } else if (req.user.admin) {
+      req.flash('warn', "Choose a client");
+      res.redirect('/admin');
+    } else {
+      clientQuery = Client.findById(req.user.client);
+    }
+    clientQuery.exec(function(err, client) {
+      if (err) {
+        console.log(err);
+      } else {
+        req.client = client;
+        req.projects = req.client.projects;
       }
       next();
     });
@@ -46,7 +72,14 @@ module.exports = function(app) {
     });
   });
 
-  app.get('/choose', auth.ensureAuthenticated, function(req, res) {
+  app.get('/choose', auth.ensureAuthenticated, getProjects, function(req, res) {
+    res.render('home/choose', {
+      title: req.client.name + ' Projects',
+      message: req.flash(),
+      theClient: req.client,
+      projects: req.projects,
+      serviceData: {}
+    });
   });
 
   // TODO: Namespace the return object per user,
