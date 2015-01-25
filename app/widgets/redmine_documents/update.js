c0c69a77a8cca31d9095b2ab963f98b3a8ce40a9
Steve Schwartz <steve@alfajango.com>
2015-01-24 19:42:33 -0500

    Add copy document URL to clipboard button for sharing direct client links to documents in redmine-documents widget.

--- app/widgets/redmine_documents/update.js
+++ app/widgets/redmine_documents/update.js
@@ -7,6 +7,10 @@ $(document)
   .delegate('.modal', 'hide', function(e) {
     var url = window.location.href.replace(/#.*/, '');
     history.pushState(null, null, url);
+  })
+  .delegate('[data-copy-url]', 'click', function(e) {
+    window.prompt("Press Cmd+C and press Enter to copy the link to this document to your clipboard:", $(e.target).attr('href'));
+    e.preventDefault();
   });
 
 
@@ -14,7 +18,8 @@ widgets.redmine_documents = function(data, $) {
   var $target = $('#widget-' + data.id),
       rows = "",
       modals = "",
-      totalDocuments;
+      totalDocuments,
+      urlWithoutClientIdOrAnchor = window.location.href.replace(/client_id=[^#&]+&?/, '').replace(/#.*/, '');
   if (data.results && data.results.length > 0) {
     totalDocuments = 0;
     $.each(data.results, function(i, category) {
@@ -25,7 +30,7 @@ widgets.redmine_documents = function(data, $) {
         rows += '<td class="redmine-title-td" data-toggle="modal" data-target="#redmine-document-' + doc.id + '"><div>' + doc.title + '</div><div class="redmine-document-description"><hr /></div></td>';
         rows += '</tr>';
 
-        modals += '<div id="redmine-document-' + doc.id + '" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="' + doc.title + '" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="redmine-document-' + doc.id + '-label">' + doc.title + '</h3></div><div class="modal-body">' + doc.description.replace(/(?:\r\n|\r|\n)/g, '<br />') + '</div></div>';
+        modals += '<div id="redmine-document-' + doc.id + '" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="' + doc.title + '" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="redmine-document-' + doc.id + '-label">' + doc.title + ' <small class="redmine-documents-share-url pull-right"><a data-copy-url href="' + urlWithoutClientIdOrAnchor + '#redmine-document-' + doc.id + '"><i class="icon-share"></i> Share Link</a></small></h3></div><div class="modal-body">' + doc.description.replace(/(?:\r\n|\r|\n)/g, '<br />') + '</div></div>';
       });
     });
   } else if (data.error) {
