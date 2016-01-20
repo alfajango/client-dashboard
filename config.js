b01dbd6a44e986747278f6d1169e819728520462
Richard Peng <richard@richardpeng.com>
2016-01-20 14:34:43 -0500

    Update to latest version of packages; Install extracted modules for Express 4.0; Update middleware configuration to use new locals syntax

--- config.js
+++ config.js
@@ -1,68 +1,85 @@
-var exports = module.exports = express = require('express'),
-    MongoStore = require('connect-mongodb-session')(express);
-    flash = require('connect-flash'),
-    middleware = require(__dirname + '/lib/middleware');
+var express = require('express'),
+  MongoStore = require('connect-mongodb'),
+  flash = require('connect-flash'),
+  errorhandler = require('errorhandler'),
+  favicon = require('serve-favicon'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  middleware = require(__dirname + '/lib/middleware');
 
 exports = module.exports = passport = require('passport');
 exports = module.exports = passwordHash = require('password-hash');
 
 module.exports = function(app, options) {
-  if (!options) { options = {}; }
+  if (!options) {
+    options = {};
+  }
 
   // Load configurations
   var config_file = require('yaml-config');
-  exports = module.exports = config = config_file.readConfig('./config.yaml')
+  exports = module.exports = config = config_file.readConfig('./config.yaml');
+  var env = process.env.NODE_ENV || 'development';
 
   // Load environment-specific config
-  app.configure('development', function(){
-    app.use(express.errorHandler());
-  });
+  if ('development' == env) {
+    app.use(errorhandler());
+  }
 
-  app.configure('production', function(){
+  if ('production' == env) {
     // TODO: Figure out a way to load config.yaml through EJS,
     // so this can be added there for production environment,
     // instead of just overriding it like this.
     config.db.uri = process.env.MONGODB_URI;
     config.app_secret = process.env.APP_SECRET;
     config.analytics_account = process.env.ANALYTICS_ACCOUNT
-  });
+  }
 
   // Connect to db and load models
   require(__dirname + '/lib/db-connect');
 
-  app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/app/views');
-    app.set('view engine', 'jade');
-    app.set('view options', { layout: __dirname + '/app/views/layouts/application' });
-    if (config.useSSL) {
-      app.use(middleware.ensureSSL);
-    }
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser());
-    // Needed because otherwise, connect-mongodb won't
-    // close the mongodb connection when jake script is done.
-    if (!options.skipSession) {
-      app.use(express.session({
-        key: 'express.sid',
-        secret: config.app_secret,
-        saveUninitialized: false,
-        resave: true,
-        maxAge: new Date(Date.now() + 3600000),
-        store: new MongoStore({
-          uri: config.db.uri,
-          collection: 'new_sessions'
-        })
-      }));
-    }
-    app.use(express.static(__dirname + '/public'));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(middleware.setLocals);
-    app.use(flash());
-    app.use(app.router);
-  });
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/app/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {layout: __dirname + '/app/views/layouts/application'});
+  if (config.useSSL) {
+    app.use(middleware.ensureSSL);
+  }
+  app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(morgan('combined'));
+  //app.use(bodyParser());
+  //app.use(bodyParser.json());
+  //app.use(bodyParser.raw());
+  //app.use(bodyParser.text());
+  //app.use(methodOverride);
+  app.use(cookieParser());
+  // Needed because otherwise, connect-mongodb won't
+  // close the mongodb connection when jake script is done.
+  if (!options.skipSession) {
+    app.use(session({
+      key: 'express.sid',
+      secret: config.app_secret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: true,
+        maxAge: new Date(Date.now() + 3600000)
+      },
+      store: new MongoStore(
+        {db: mongoose.connection.db},
+        function(err) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      )
+    }));
+  }
+  app.use(express.static(__dirname + '/public'));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(middleware.setLocals);
+  app.use(flash());
 };
