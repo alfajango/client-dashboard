248ca0f70be35b992d0779bdc10d26e4c8dd3fec
Jonathan Gabel <hello@jonathangabel.com>
2016-01-21 10:18:57 -0500

    update bodyParser call
        bodyParser() is now deprecated, we need to call individual encodings
        see: https://github.com/expressjs/body-parser/commit/b7420f8dc5c8b17a277c9e50d72bbaf3086a3900

--- config.js
+++ config.js
@@ -49,7 +49,10 @@ module.exports = function(app, options) {
   }
   app.use(favicon(__dirname + '/public/favicon.ico'));
   app.use(morgan('combined'));
-  app.use(bodyParser());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
   app.use(methodOverride(function(req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
