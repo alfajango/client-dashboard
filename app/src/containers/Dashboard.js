ca240647a87b5604111589d2980836238d02ea21
Jonathan Gabel <hello@jonathangabel.com>
2016-02-19 09:51:35 -0500

    remove react grid layout

--- app/src/containers/Dashboard.js
+++ app/src/containers/Dashboard.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react'
-import { WidthProvider, Responsive } from 'react-grid-layout'
 import Invoices from '../../widgets/invoices/widget'
 import Payments from '../../widgets/payments/widget'
-const GridLayout = WidthProvider(Responsive);
 
-var Dashboard = React.createClass({
-  createComponents() {
-    return window.services.map(function(s) {
-      return React.createElement('div', {key: s.id},
-        React.createElement(widgetToComponent(s.widget), {id: s.id}))
-    })
-  },
-
+class Dashboard extends Component {
   render() {
-    var layouts = {
-      lg: window.services.map(function(s, i) {
-        return {x: i % 2, y: 0, w: 1, h: 1, i: s.id}
-      })
-    };
-
     return (
-      <GridLayout className="layout" layouts={layouts} breakpoints={{lg: 0}} cols={{lg: 2}} rowHeight={500}>
-        {this.createComponents()}
-      </GridLayout>
+      <div>
+        {createComponents()}
+      </div>
     )
   }
-});
+};
 
 function widgetToComponent(widget) {
   switch (widget) {
@@ -34,6 +19,12 @@ function widgetToComponent(widget) {
     case 'payments':
       return Payments;
   }
+}
+
+function createComponents() {
+  return window.services.map(function(s) {
+    return React.createElement(widgetToComponent(s.widget), {id: s.id, key: s.id})
+  })
 }
 
 export default Dashboard
