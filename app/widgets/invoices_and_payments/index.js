ec0d1a27a0cd8aee5467e284878bc369b957c220
Jonathan Gabel <hello@jonathangabel.com>
2016-02-19 11:37:10 -0500

    implement invoices payments client api call

---
+++ app/widgets/invoices_and_payments/index.js
@@ -0,0 +1,4 @@
+exports.invoices_and_payments = {
+  name: "Invoices and Payments",
+  model: require('./model.js')
+};
---
+++ app/widgets/invoices_and_payments/widget.js
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import InvoiceList from '../../src/components/InvoiceList'
+
+class Widget extends Component {
+  render() {
+    const { data, status, isFetching } = this.props;
+    return (
+      <div>
+        <h2>Invoices</h2>
+        {isFetching &&
+          React.createElement('div', {className: 'loading large'},
+            status,
+            ' ',
+            React.createElement('img', {src: '/images/ajax-loader.gif'}))
+        }
+        {!isFetching &&
+        <InvoiceList>{data}</InvoiceList>
+        }
+      </div>
+    )
+  }
+}
+
+Widget.propTypes = {
+  isFetching: PropTypes.bool,
+  status: PropTypes.string,
+  data: PropTypes.array
+};
+
+function mapStateToProps(state, ownProps) {
+  const {
+    isFetching,
+    status,
+    data,
+    } = state.dataByService[ownProps.id] || {
+    isFetching: true,
+    status: 'Loading',
+    data: []
+  };
+
+  return {
+    isFetching,
+    status,
+    data
+  }
+}
+
+export default connect(mapStateToProps)(Widget)
