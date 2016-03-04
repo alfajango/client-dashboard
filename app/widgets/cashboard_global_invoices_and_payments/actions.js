73ae46e88a0c5a7ae70c83ddcdc7ad3dd51a6ae5
Richard Peng <richard@richardpeng.com>
2016-03-04 17:38:46 -0500

    Handle clearing and selecting new client in Invoices and Payments dropdown

--- app/widgets/cashboard_global_invoices_and_payments/actions.js
+++ app/widgets/cashboard_global_invoices_and_payments/actions.js
@@ -1,10 +1,11 @@
-import { requestData } from '../../src/actions'
+import { requestData, invalidateData } from '../../src/actions'
 
 export const SELECT_CLIENT = 'SELECT_CLIENT';
 
 export function selectClient(serviceName, serviceId, clientId) {
   return function(dispatch) {
-    dispatch(requestData(serviceId));
+    dispatch(invalidateData(serviceId));
+    dispatch(requestData(serviceId, {clientId: clientId}));
     emitters[serviceName + '-' + serviceId]({clientId: clientId});
   };
 }
