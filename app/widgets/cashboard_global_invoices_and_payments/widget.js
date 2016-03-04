344e7e67d6f15f7672f175629928da2490b44a94
Richard Peng <richard@richardpeng.com>
2016-03-04 17:38:46 -0500

    Handle clearing and selecting new client in Invoices and Payments dropdown

--- app/widgets/cashboard_global_invoices_and_payments/widget.js
+++ app/widgets/cashboard_global_invoices_and_payments/widget.js
@@ -3,45 +3,43 @@ import { connect } from 'react-redux'
 import Select from 'react-select'
 import 'style!react-select/scss/default.scss'
 import { selectClient } from './actions'
+import { invalidateData } from '../../src/actions'
 import InvoiceList from '../../src/components/InvoiceList'
 import PaymentList from '../../src/components/PaymentList'
 
 var Widget = React.createClass({
-  getInitialState() {
-    // TODO: track these in state
-    return {
-      clientId: ""
+  selectClient(client) {
+    if (client) {
+      this.props.selectClient(this.props.name, this.props.id, client.value);
+    } else {
+      this.props.clearClient(this.props.id, {client: this.props.data.client});
     }
   },
 
-  selectClient(client) {
-    this.state.clientId = client.value;
-    this.props.selectClient(this.props.name, this.props.id, client.value);
-  },
-
   render() {
-    const { data, status, isFetching } = this.props;
+    const { data, status, isFetching, didInvalidate } = this.props;
     return (
       <div>
         <h2>Invoices and Payments</h2>
+        {data.client &&
+        <Select
+          autofocus
+          value={data.clientId}
+          options={data['client'].map(i => ({value:i.id,label:i.attributes.name}))}
+          onChange={this.selectClient}
+          isLoading={!data.client}
+        />
+        }
         {isFetching &&
         React.createElement('div', {className: 'loading large'},
           status,
           ' ',
           React.createElement('img', {src: '/images/ajax-loader.gif'}))
         }
-        {!isFetching &&
-        <Select
-          autofocus
-          value={this.state.clientId}
-          options={data['client'].map(i => ({value:i.id,label:i.attributes.name}))}
-          onChange={this.selectClient}
-        />
-        }
-        {this.state.clientId && data.invoice &&
+        {!didInvalidate && data.clientId && data.invoice &&
         <InvoiceList>{data['invoice']}</InvoiceList>
         }
-        {this.state.clientId && data.payment &&
+        {!didInvalidate && data.clientId && data.payment &&
         <PaymentList>{data['payment']}</PaymentList>
         }
       </div>
@@ -60,6 +58,9 @@ const mapDispatchToProps = (dispatch) => {
   return {
     selectClient: (serviceName, serviceId, clientId) => {
       dispatch(selectClient(serviceName, serviceId, clientId));
+    },
+    clearClient: (serviceId, data) => {
+      dispatch(invalidateData(serviceId, data));
     }
   }
 };
@@ -69,16 +70,24 @@ const mapStateToProps = (state, ownProps) => {
 
   const {
     isFetching,
+    didInvalidate,
     status,
     data
     } = state.dataByService[ownProps.id] || {
     isFetching: true,
+    didInvalidate: false,
     status: 'Loading',
-    data: {}
+    data: {
+      clientId: ''
+    }
   };
+  if (didInvalidate) {
+    data.clientId = '';
+  }
 
   return {
     isFetching,
+    didInvalidate,
     status,
     data,
     id
