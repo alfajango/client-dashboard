b6e36efa8e7e46d2e7a2b03ba3d430750261152d
Richard Peng <richard@richardpeng.com>
2016-03-04 18:19:48 -0500

    Set clientID and clear out invoices and payment data when selecting a new client

--- app/widgets/cashboard_global_invoices_and_payments/actions.js
+++ app/widgets/cashboard_global_invoices_and_payments/actions.js
@@ -2,10 +2,9 @@ import { requestData, invalidateData } from '../../src/actions'
 
 export const SELECT_CLIENT = 'SELECT_CLIENT';
 
-export function selectClient(serviceName, serviceId, clientId) {
+export function selectClient(serviceName, serviceId, data) {
   return function(dispatch) {
-    dispatch(invalidateData(serviceId));
-    dispatch(requestData(serviceId, {clientId: clientId}));
-    emitters[serviceName + '-' + serviceId]({clientId: clientId});
+    dispatch(requestData(serviceId, data));
+    emitters[serviceName + '-' + serviceId]({clientId: data.clientId});
   };
 }
