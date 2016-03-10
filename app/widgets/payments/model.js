908c45a3f45016d2cc79ac85e03d4d2cdbf8e383
Richard Peng <richard@richardpeng.com>
2016-03-10 15:14:50 -0500

    Refactor invoice and payment widgets to use ioutils library

--- app/widgets/payments/model.js
+++ app/widgets/payments/model.js
@@ -1,107 +1,41 @@
-var https = require('https');
-
-exports.options = function(service, path) {
-  var auth = 'Basic ' + new Buffer(service.user + ':' + service.token).toString('base64');
-  return {
-    host: service.url || 'api.cashboardapp.com',
-    port: 443,
-    path: path,
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': auth
-    }
-  };
-};
+var ioutils = require('../ioutils');
 
 exports.fetch = function(service, callback) {
-  var widget = this,
-    response = {
-      serviceId: service.id
-    },
-    projectId = service.identifier;
+  var widget = this;
+  var fetchAPI = ioutils.createFetchAPI('https', options(service));
+  var io = ioutils.updates(service.id, callback);
 
   if (service.identifier === '') {
-    updateError('Missing service identifier clientId')
+    io.updateError('Missing service identifier clientId')
   } else {
     var projectData = {};
     utils.when(
       function(done) {
-        updateStatus('Looking up project');
+        io.updateStatus('Looking up project');
         var path = '/projects/'+service.identifier;
-        widget.fetchAPI('project', service, path, projectData, done);
+        fetchAPI('project', path, projectData, done);
       }
     ).then(function() {
       if (projectData.error) {
-        updateError('projectId was not found')
+        io.updateError('projectId was not found')
       } else {
         var data = {};
         utils.when(
           function(done) {
-            updateStatus('Loading payments');
+            io.updateStatus('Loading payments');
             var path = '/payments.json?client_type=Company&client_id=' + projectData.data.client_id;
-            widget.fetchAPI('payments', service, path, data, done);
+            fetchAPI('payments', path, data, done);
           }
         ).then(function() {
           if (data.error) {
-            updateError(data.error);
+            io.updateError(data.error);
           } else {
-            updateData(widget.translate(data.data));
+            io.updateData(widget.translate(data.data));
           }
         });
       }
     })
   }
-
-  function updateData(data) {
-    callback(Object.assign({}, response, {
-      data: data
-    }))
-  }
-
-  function updateError(msg) {
-    callback(Object.assign({}, response, {
-      error: msg
-    }));
-  }
-
-  function updateStatus(msg) {
-    callback(Object.assign({}, response, {
-      status: msg
-    }));
-  }
-};
-
-// Fetch issues from service endpoint
-exports.fetchAPI = function(name, service, path, jsonData, done) {
-  var options = this.options(service, path);
-  console.log('GETTING: ' + name);
-
-  var req = https.get(options, function(res) {
-    if (res.statusCode == 200) {
-      var data = "";
-      res.on('data', function(chunk) {
-        data += chunk;
-      });
-      res.on('end', function() {
-        try {
-          jsonData.data = JSON.parse(data);
-        } catch (err) {
-          console.log("Got a parsing error: " + err.message);
-          jsonData.error = err.message;
-        }
-        console.log('DONE: ' + name);
-        done();
-      });
-    } else {
-      console.log('Status code: ' + res.statusCode);
-      jsonData.error = res.statusCode;
-      done();
-    }
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-    jsonData.error = e.message;
-    done();
-  });
 };
 
 // Translate fetched response to db store format
@@ -123,6 +57,14 @@ exports.translate = function(data) {
   }
 };
 
-// Write fetched results to db
-exports.write = function() {
-};
+function options(service) {
+  var auth = 'Basic ' + new Buffer(service.user + ':' + service.token).toString('base64');
+  return {
+    host: service.url || 'api.cashboardapp.com',
+    port: 443,
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': auth
+    }
+  };
+}
