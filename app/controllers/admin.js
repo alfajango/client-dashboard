b2c58a06cdcb1bdeb42031abb86f2fadfec4538d
Alec Tower <alectower@gmail.com>
2016-11-22 10:23:00 -0500

    Fix service-update-endpoint by using PUT request

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -284,7 +284,7 @@ module.exports = function(app) {
     });
   });
 
-  app.post('/admin/clients/:id/projects/:project_id/services/:service_id', auth.ensureAuthenticated, auth.ensureAdmin, function(req, res) {
+  app.put('/admin/clients/:id/projects/:project_id/services/:service_id', auth.ensureAuthenticated, auth.ensureAdmin, function(req, res) {
     for (attr in req.body.service) {
       req.service[attr] = req.body.service[attr];
     }
