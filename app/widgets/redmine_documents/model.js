38435cd0e75909bfbf65175b4bbcadf6bdc0964e
Steve Schwartz <steve@alfajango.com>
2021-02-04 22:22:18 -0500

    Updated service parent and parent parent document which now returns as a function instead of an attribute with newer mongo packages.

--- app/widgets/redmine_documents/model.js
+++ app/widgets/redmine_documents/model.js
@@ -113,7 +113,7 @@ exports.translate = function(data, service) {
             .replace(/{{video\(https?:\/\/(www\.)?youtu(be\.com|\.be)\/(watch\?.*v=)?([-\d\w]+)[^}]*}}/, '<iframe width="100%" height="400" src="//www.youtube-nocookie.com/embed/$4?rel=0" frameborder="0" allowfullscreen></iframe>')
             .replace(/!(\/[^\s]+)!/gm, function(match, p1) {
               var path = encodeURIComponent(p1);
-              return ("<img src='/proxy/redmine_documents?service_id=" + service.id + "&project_id=" + service.parent.id + "&client_id=" + service.parent.parent.id + "&proxy=image&path=" + path + "' />");
+              return ("<img src='/proxy/redmine_documents?service_id=" + service.id + "&project_id=" + service.parent().id + "&client_id=" + service.parent().parent().id + "&proxy=image&path=" + path + "' />");
             })
             .replace(/((^>.*$(\r\n)?)+)/gm, "<blockquote>$1</blockquote>")
             .replace(/^(<blockquote>)?> +$/gm, "$1&nbsp;")
